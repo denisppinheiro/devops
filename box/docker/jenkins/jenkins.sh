597fe53ae0b12215eaf05d9c206a9423aabfdf9c
Denis Pinheiro <denisppinheiro@gmail.com>
2017-06-28 02:47:43 -0300

    Adding Jenkins provisioning

--- box/docker/jenkins/jenkins.sh
+++ box/docker/jenkins/jenkins.sh
@@ -4,7 +4,7 @@ echo "DevOps Box: provisioning Jenkins"
 
 if ! [ $(sudo docker restart jenkins) ]; then
   
-	sudo docker run --name jenkins -p 9280:8080 -p 50000:50000 -v /vagrant/docker/jenkins/.box:/var/jenkins_home jenkins
+	sudo docker run --name jenkins -p 9280:8080 -p 50000:50000 -v /vagrant/docker/jenkins/.box:/var/jenkins_home jenkins &
 	
 else
 
